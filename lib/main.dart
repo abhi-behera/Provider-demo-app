5cd96af68c8da966802f70c39cf08618e01a027f
abhi-behera <abhijitbehera1997@gmail.com>
2024-10-04 12:02:21 +0530

    basic provider code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(FutureProvider<int?>(create:((context)=> Future.value(20)),
+  initialData: null,
+  child: const MyApp()));
+
 }
 
 class MyApp extends StatelessWidget {
@@ -11,11 +15,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Provider Demo App',
       theme: ThemeData( colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Provider Demo Page'),
     );
   }
 }
@@ -30,16 +34,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
+  int  _counter =  0;
   void _incrementCounter() {
-    setState(() {
+    // setState(() {
       _counter++;
-    });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
+   final counter = context.watch<int?>();
     return Scaffold(
       appBar: AppBar(
        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -53,7 +57,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              '$counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
